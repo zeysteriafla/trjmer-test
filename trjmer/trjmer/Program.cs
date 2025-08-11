96ba28ee78de5e4350f4aded50119c8bb28ca12a
zeysteriafla <epicvillegamer@gmail.com>
2025-08-11 12:12:42 +0200

    trjmer test
    I'm not responsible for any damage. Run in a virtual machine only, this does not overwrite your MBR

--- trjmer/trjmer/Program.cs
+++ trjmer/trjmer/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace trjmer
+namespace gdi_test
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +15,7 @@ namespace trjmer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(mainForm: new Form1());
+            Application.Run(new gdi());
         }
     }
 }
